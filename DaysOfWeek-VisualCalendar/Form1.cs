f4abee23a922c46dd87a6a912a0b071fc92a9df7
Gustavo Mariano <47873305+GustavoMariano@users.noreply.github.com>
2022-04-21 04:28:18 -0300

    Visual calendar showing tasks for each day of the week.

---
+++ DaysOfWeek-VisualCalendar/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace DaysOfWeek_VisualCalendar
+{
+    public partial class Form1 : Form
+    {
+        private readonly DateTime date;
+        public Form1()
+        {
+            date = DateTime.Now;
+            InitializeComponent();
+            SetLabelsWithDaysOfWeek();
+            DisplayTasks();
+        }
+
+        private void DisplayTasks()
+        {
+            for (int i = 0; i < 7; i++)
+            {
+                DateTime dateToSelecthInDB = date.AddDays(i);
+                UserControlDays userCDays = new(dateToSelecthInDB);
+                flowLayoutPanel1.Controls.Add(userCDays);
+            }
+        }
+
+        private void SetLabelsWithDaysOfWeek()
+        {
+            label1.Text = date.AddDays(0).DayOfWeek.ToString();
+            label2.Text = date.AddDays(1).DayOfWeek.ToString();
+            label3.Text = date.AddDays(2).DayOfWeek.ToString();
+            label4.Text = date.AddDays(3).DayOfWeek.ToString();
+            label5.Text = date.AddDays(4).DayOfWeek.ToString();
+            label6.Text = date.AddDays(5).DayOfWeek.ToString();
+            label7.Text = date.AddDays(6).DayOfWeek.ToString();
+        }
+    }
+}
---
+++ DaysOfWeek-VisualCalendar/UserControlDays.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace DaysOfWeek_VisualCalendar
+{
+    public partial class UserControlDays : UserControl
+    {
+        public UserControlDays(DateTime date) //Date to select Tasks in DB
+        {
+            InitializeComponent();
+
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.Columns.AddRange(new DataGridViewTextBoxColumn { 
+                DataPropertyName = "Task", HeaderText = "Task" }
+            );
+            dataGridView1.Columns[0].DefaultCellStyle.Format = "MM/dd/yyyy";
+            dataGridView1.Rows.Add($"Test {date}");
+        }
+    }
+}
